97b816f31d41360ddcf2bc449e41cd95932167c4
Nuclear451 <Koryakovsky.Ivan@gmail.com>
2019-04-28 17:05:25 +0500

    SPA

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import Menu from './MenuComponent'
+import Contact from './ContactComponent'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
 import {DISHES} from "../shared/dishes";
+import {COMMENTS} from "../shared/comments";
+import {LEADERS} from "../shared/leaders";
+import {PROMOTIONS} from "../shared/promotions";
+
+
 
 import {Redirect, Route, Switch} from "react-router-dom";
 
@@ -14,13 +20,20 @@ class Main extends Component {
 
         this.state = {
             dishes:DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         };
     }
 
     render() {
         const HomePage = () => {
             return(
-                <Home/>
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         };
 
@@ -31,6 +44,7 @@ class Main extends Component {
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() =>
                         <Menu dishes={this.state.dishes}/>} />
+                    <Route exact path="/contactus" component={Contact}/>
                     <Redirect to="/home" />
                 </Switch>
                 <Footer/>
